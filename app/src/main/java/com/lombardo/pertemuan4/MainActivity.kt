10bf0c0496a6c82450ec3415e1d062990ea7f631
Lombardo Donata Harianja <lombardo18si@mahasiswa.pcr.ac.id>
2021-01-18 13:59:45 +0700

    LAYOUT

--- app/src/main/java/com/lombardo/pertemuan4/MainActivity.kt
+++ app/src/main/java/com/lombardo/pertemuan4/MainActivity.kt
@@ -2,10 +2,18 @@ package com.lombardo.pertemuan4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
